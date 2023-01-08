27c05070019f4d9e2522d3dae9a6847fa0416e62
Joshua Hunsberger <joshua@hunsberger.co>
2023-01-08 14:45:01 -0500

    Add code to check for APIM user and create if not exists

--- AzureApimPlayground.Console/Program.cs
+++ AzureApimPlayground.Console/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.ApiManagement;
+using Azure.ResourceManager.ApiManagement.Models;
 
+// Can authenticate with az login
+var armClient = new ArmClient(new DefaultAzureCredential());
+
+// TODO: Add resource ID for APIM subscription
+var resourceId = new ResourceIdentifier("");
+
+var serviceResource = armClient.GetApiManagementServiceResource(resourceId);
+
+var userCollection = serviceResource.GetApiManagementUsers();
+
+// TODO: Add desired email
+const string newUserEmail = "";
+var existingUserResults = userCollection.GetAll($"email eq '{newUserEmail}'", top: 1);
+var user = existingUserResults.FirstOrDefault();
+
+if (user == null)
+{
+    var createContent = new ApiManagementUserCreateOrUpdateContent
+    {
+        Email = newUserEmail,
+        FirstName = "Joshua",
+        LastName = "Hunsberger",
+    };
+
+    var userOperation = await userCollection.CreateOrUpdateAsync(Azure.WaitUntil.Completed, Guid.NewGuid().ToString(), createContent, false);
+    user = userOperation.Value;
+}
+
+if (user == null)
+{
+    Console.WriteLine("Unable to find or create user");
+    Environment.Exit(-1);
+}
