5cdcab8ee038742742dfd952a322f6e321ad8c99
Joshua Hunsberger <joshua@hunsberger.co>
2023-01-08 17:14:09 -0500

    Check for and create user subscription for APIM product

--- AzureApimPlayground.Console/AzureSettings.cs
+++ AzureApimPlayground.Console/AzureSettings.cs
@@ -3,4 +3,9 @@
     public string AzureSubscriptionId { get; set; }
     public string ApimResourceGroupName { get; set; }
     public string ApimServiceName { get; set; }
+    public string ApimProductName { get; set; }
+
+    public string ApimResourceId => $"/subscriptions/{AzureSubscriptionId}/resourceGroups/{ApimResourceGroupName}/providers/Microsoft.ApiManagement/service/{ApimServiceName}";
+
+    public string ApimProductResourceId => $"{ApimResourceId}/products/{ApimProductName}";
 }
--- AzureApimPlayground.Console/Program.cs
+++ AzureApimPlayground.Console/Program.cs
@@ -19,8 +19,7 @@ var armClient = new ArmClient(new DefaultAzureCredential());
 var azureSettings = new AzureSettings();
 configRoot.Bind("AzureSettings", azureSettings);
 
-var apimResourceIdBase = $"/subscriptions/{azureSettings.AzureSubscriptionId}/resourceGroups/{azureSettings.ApimResourceGroupName}/providers/Microsoft.ApiManagement/service/{azureSettings.ApimServiceName}";
-var resourceId = new ResourceIdentifier(apimResourceIdBase);
+var resourceId = new ResourceIdentifier(azureSettings.ApimResourceId);
 
 var serviceResource = armClient.GetApiManagementServiceResource(resourceId);
 
@@ -47,4 +46,33 @@ if (user == null)
 {
     Console.WriteLine("Unable to find or create user");
     Environment.Exit(-1);
+}
+
+// Check for existing subscription
+var userSubscriptionCollection = user.GetApiManagementUserSubscriptions();
+
+var existingSubscriptionPages = userSubscriptionCollection.GetAll();
+var userSubscriptions = existingSubscriptionPages.ToList();
+
+var existingSubscription = userSubscriptions
+    .Where(us => us.Data.Scope.Equals(azureSettings.ApimProductResourceId, StringComparison.InvariantCultureIgnoreCase))
+    .FirstOrDefault();
+
+// Create subscription
+if (existingSubscription == null)
+{
+    var subscriptionCollection = serviceResource.GetApiManagementSubscriptions();
+    var subscriptionCreateContent = new ApiManagementSubscriptionCreateOrUpdateContent
+    {
+        OwnerId = user.Id,
+        Scope = azureSettings.ApimProductResourceId,
+        State = SubscriptionState.Active,
+        DisplayName = ".NET SDK Created Product Demo Conference API subscription"
+    };
+    var subscription = await subscriptionCollection.CreateOrUpdateAsync(Azure.WaitUntil.Completed, Guid.NewGuid().ToString(), subscriptionCreateContent, true);
+    Console.WriteLine($"Keys:\n\t{subscription.Value.Data.PrimaryKey}\n\t{subscription.Value.Data.SecondaryKey}");
+}
+else
+{
+    Console.WriteLine("Subscription already exists.");
 }
