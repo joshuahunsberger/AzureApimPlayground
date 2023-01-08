9c84ee68e9fafbcec106e0db773d181c3ae295ad
Joshua Hunsberger <joshua@hunsberger.co>
2023-01-08 15:04:52 -0500

    Add user secrets to get values from configuration

--- AzureApimPlayground.Console/Program.cs
+++ AzureApimPlayground.Console/Program.cs
@@ -3,19 +3,26 @@ using Azure.Identity;
 using Azure.ResourceManager;
 using Azure.ResourceManager.ApiManagement;
 using Azure.ResourceManager.ApiManagement.Models;
+using Microsoft.Extensions.Configuration;
+
+var configManager = new ConfigurationManager()
+    .AddUserSecrets<Program>();
+
+var configRoot = configManager
+    .Build();
 
 // Can authenticate with az login
 var armClient = new ArmClient(new DefaultAzureCredential());
 
-// TODO: Add resource ID for APIM subscription
-var resourceId = new ResourceIdentifier("");
+var subscriptionId = configRoot["AzureSubscriptionId"];
+var apimResourceIdString = $"/subscriptions/{subscriptionId}/resourceGroups/apim-playground/providers/Microsoft.ApiManagement/service/apim-playground-hunsberger";
+var resourceId = new ResourceIdentifier(apimResourceIdString);
 
 var serviceResource = armClient.GetApiManagementServiceResource(resourceId);
 
 var userCollection = serviceResource.GetApiManagementUsers();
 
-// TODO: Add desired email
-const string newUserEmail = "";
+var newUserEmail = configRoot["NewUserEmail"];
 var existingUserResults = userCollection.GetAll($"email eq '{newUserEmail}'", top: 1);
 var user = existingUserResults.FirstOrDefault();
 
